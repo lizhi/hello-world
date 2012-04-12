ab7622e3806708b4f91ac74054b9c78c9aad960d
= <leezzz@163.com>
2012-04-12 17:06:47 +0800

    add main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+/**
+* main.c
+* author: lizhi 
+* create: 2012.4.12
+* modify: 2012.4.12
+* version: 1.0
+*/
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello world.\n");
+	return 0;
+}
