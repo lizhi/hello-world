8fc4bb6c215a8ad6c95f2be22037bad6f5fe0059
= <leezzz@163.com>
2012-04-12 19:00:23 +0800

    add arguments processing.

--- main.c
+++ main.c
@@ -7,9 +7,21 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
-	printf("hello world.\n");
+	//printf("hello world.\n");
+	if (argc < 2)
+	{
+		printf("at least 2 arguments.\n");
+		return -1;
+	}
+
+	if (strcmp(argv[1], "-v"))
+	{
+		printf("hello world.\n");
+	}
+
 	return 0;
 }
