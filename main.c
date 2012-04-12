fff706bf4bc135ee0ce1cc052b4f24b76d69ccdb
= <leezzz@163.com>
2012-04-12 19:58:01 +0800

    story 4#, add argument -h

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char **argv)
 {
-	if (argc < 2)
+	if (argc < 3)
 	{
 		printf("at least 2 arguments.\n");
 		return -1;
@@ -20,6 +20,11 @@ int main(int argc, char **argv)
 	if (strcmp(argv[1], "-v"))
 	{
 		printf("hello world.\n");
+	}
+
+	if (strcmp(argv[2], "-h"))
+	{
+		printf("a.exe -v -h");
 	}
 
 	return 0;
